cbf40e42612570461f4efd24898cf9529f0d86b2
Sagar Mathur <s_mathur4@u.pacific.edu>
2022-10-31 15:07:42 -0700

    committed and pushed win/lose

---
+++ src/main/java/edu/pacific/comp55/starter/WinLose.java
@@ -0,0 +1,14 @@
+package edu.pacific.comp55.starter;
+
+public class WinLose {
+    private boolean win;
+    public void checkForWin() {
+    	
+    }
+    public void checkForLose() {
+    	
+    }
+    public void setWin() {
+    	
+    }
+}
