64dab02fa4ac738fd2e97424055af9887864b9a1
Brendon <b_amino@u.pacific.edu>
2022-11-02 12:55:29 -0700

    Imported packages and created variables.

--- src/main/java/edu/pacific/comp55/starter/Clock.java
+++ src/main/java/edu/pacific/comp55/starter/Clock.java
@@ -1,5 +1,15 @@
 package edu.pacific.comp55.starter;
+import javax.swing.*;
 
+import acm.graphics.GLabel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+//Brendon Amino
 public class Clock {
-
+	private String s;
+	private GLabel label;
+	//Timer someTimerVar = new Timer(1000);
 }
