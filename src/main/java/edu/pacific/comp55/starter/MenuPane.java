9efd253a7b7d386d06f3df2e545617b99149c1bf
ayeshashamsi <a_shamsi@u.pacific.edu>
2022-11-07 15:02:33 -0800

    made quit Gbutton for main menu

--- src/main/java/edu/pacific/comp55/starter/MenuPane.java
+++ src/main/java/edu/pacific/comp55/starter/MenuPane.java
@@ -10,13 +10,16 @@ public class MenuPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
 	private GButton rect;
+	private GButton rect2;
 	private GImage background;
 
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
 		rect = new GButton("Start", 730, 650, 150, 100);
+		rect2 = new GButton("Quit", 400, 650, 150, 100);
 		rect.setFillColor(Color.GREEN);
+		rect2.setFillColor(Color.RED);
 		background = new GImage("2.png", 0, 0);
 	}
 
@@ -24,6 +27,7 @@ public class MenuPane extends GraphicsPane {
 	public void showContents() {
 		program.add(background);
 		program.add(rect);
+		program.add(rect2);
 	}
 
 	@Override
