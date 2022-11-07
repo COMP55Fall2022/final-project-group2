58ca35779a1d99492fbc2d37c07b0e47532d5a81
Sagar Mathur <s_mathur4@u.pacific.edu>
2022-11-07 14:42:05 -0800

    setting win and lose labels

--- src/main/java/edu/pacific/comp55/starter/WinLose.java
+++ src/main/java/edu/pacific/comp55/starter/WinLose.java
@@ -1,15 +1,33 @@
 package edu.pacific.comp55.starter;
 
+import acm.graphics.*;
+import acm.util.*;
+import java.awt.*;
+import acm.program.*;
+
+//Sagar Mathur
 public class WinLose {
     private boolean win;
+    
+    private GLabel winLabel;
+    private GLabel loseLabel;
     public void setWin(boolean w) {
     	w = win;
     }
     public boolean checkForWin() {
+    	
     	return true;
     }
     public boolean checkForLose() {
     	return false;
+    }
+    public void setWinLabel() {
+    	winLabel = new GLabel("YOU WIN!!!", 50, 50);
+    	
+    }
+    public void setLoseLabel() {
+    	loseLabel = new GLabel("YOU LOSE!!!",50,50);
+    	
     }
     
 }
