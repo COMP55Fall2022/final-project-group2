f6fb16d9b90a7353d7c414863d4ae5b032bf25ba
ayeshashamsi <a_shamsi@u.pacific.edu>
2022-11-07 14:41:57 -0800

    added menu back ground in menu game

--- src/main/java/edu/pacific/comp55/starter/MenuPane.java
+++ src/main/java/edu/pacific/comp55/starter/MenuPane.java
@@ -3,23 +3,27 @@ package edu.pacific.comp55.starter;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GImage;
 import acm.graphics.GObject;
 
 public class MenuPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
 	private GButton rect;
+	private GImage background;
 
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
 		rect = new GButton("Next", 200, 200, 200, 200);
 		rect.setFillColor(Color.RED);
+		background = new GImage("2.png", 0, 0);
 	}
 
 	@Override
 	public void showContents() {
-		program.add(rect);
+		//program.add(rect);
+		program.add(background);
 	}
 
 	@Override
