32abfd235d0313412bf19cdc03a4bf81297ba60b
Sagar Mathur <s_mathur4@u.pacific.edu>
2022-11-07 21:49:39 -0800

    Started tic tac toe

---
+++ src/main/java/edu/pacific/comp55/starter/TicTacToe.java
@@ -0,0 +1,9 @@
+package edu.pacific.comp55.starter;
+import javax.swing.*;
+
+
+public class TicTacToe {
+     private char board[][] = new char[3][3];
+     private char xPlayer;
+     private char yPlayer;
+}
