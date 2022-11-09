feec2b889227bb86a82550f05f7052271298f6f2
Sagar Mathur <s_mathur4@u.pacific.edu>
2022-11-09 15:12:35 -0800

    Printing tic tac toe start

--- src/main/java/edu/pacific/comp55/starter/TicTacToe.java
+++ src/main/java/edu/pacific/comp55/starter/TicTacToe.java
@@ -3,16 +3,45 @@ import javax.swing.*;
 
 
 public class TicTacToe {
-     private char board[][] = new char[3][3];
-     private char xPlayer;
-     private char yPlayer;
+     public char[][] board;
+     private int rows;
+     private int cols;
+     
+     //private char xPlayer;
+     //private char yPlayer;
+     
+     public TicTacToe(int rows, int cols) {
+    	 this.rows = rows;
+    	 this.cols = cols;
+    	 this.board = new char[rows][cols];
+     }
      public void setupBoard() {
-    	 for (int i = 0; i < 3; i++) {
-    		 for(int j = 0; j < 3; j++) {
+    	 for (int i = 0; i < rows; i++) {
+    		 for(int j = 0; j < cols; j++) {
     			 board[i][j] = 'e';
+    			 
     		 }
     	 }
-    	 System.out.print(board);
+    	 
+     }
+     public void printBoard() {
+    	 for(int i = 0; i < rows; i++) {
+    		 
+    		 for(int j = 0; j < cols; j++) {
+    			 System.out.println(board[i][j]);
+    		 }
+    		 System.out.println("\n");
+    	 }
+     }
+     public static void main(String[] args) {
+    	 TicTacToe t = new TicTacToe(3,3);
+    	 t.setupBoard();
+    	 t.printBoard();
      }
      
-}
+}   
+
+
+	
+
+
