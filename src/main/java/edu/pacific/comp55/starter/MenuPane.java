26ba137ea40bbbb05b53b03a3dd86d0442a2f6e9
Sagar Mathur <s_mathur4@u.pacific.edu>
2022-11-09 14:19:33 -0800

    Fixed merge conflict

--- src/main/java/edu/pacific/comp55/starter/MenuPane.java
+++ src/main/java/edu/pacific/comp55/starter/MenuPane.java
@@ -12,14 +12,17 @@ public class MenuPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
 	private GButton rect;
+	private GButton rect2;
 	private GImage background;
 	private GLabel title;
 
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
-		rect = new GButton("Next", 200, 200, 200, 200);
-		rect.setFillColor(Color.RED);
+		rect = new GButton("Start", 730, 650, 150, 100);
+		rect2 = new GButton("Quit", 400, 650, 150, 100);
+		rect.setFillColor(Color.GREEN);
+		rect2.setFillColor(Color.RED);
 		background = new GImage("2.png", 0, 0);
 		title = new GLabel("SAYA'S BIG ADVENTURE", 300, 300);
 		title.setFont("TimesRoman");
@@ -30,14 +33,17 @@ public class MenuPane extends GraphicsPane {
 
 	@Override
 	public void showContents() {
-		//program.add(rect);
 		program.add(background);
 		program.add(title);
+		program.add(rect);
+		program.add(rect2);
+
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(rect);
+		program.remove(rect2);
 	}
 
 	@Override
@@ -46,5 +52,12 @@ public class MenuPane extends GraphicsPane {
 		if (obj == rect) {
 			program.switchToSome();
 		}
+		else if (obj == rect2){
+			System.exit(0);
+		}
+		else {
+			
+		}
 	}
+	
 }
