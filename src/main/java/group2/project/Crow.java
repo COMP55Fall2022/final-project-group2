7fb0d340eae450fd343a16608744180cc689bb47
ayeshashamsi <a_shamsi@u.pacific.edu>
2022-11-11 14:21:55 -0800

    added crowgame to other file

--- src/main/java/group2/project/Crow.java
+++ src/main/java/group2/project/Crow.java
@@ -1,4 +1,4 @@
-package edu.pacific.comp55.starter;
+package group2.project;
 
 import javax.swing.Timer;
 import java.awt.Color;
@@ -9,18 +9,21 @@ import java.awt.event.KeyListener;
 
 import acm.graphics.GImage;
 import acm.graphics.GOval;
+import edu.pacific.comp55.starter.GraphicsPane;
+import edu.pacific.comp55.starter.MainApplication;
 
 
 public class Crow extends GraphicsPane implements ActionListener, KeyListener {
 	private static final int START_Y = 675;
-	private static final int START_X = 0;
+	private static final int START_X = 550;
 	private MainApplication program;
 	private GImage crowgamebackground;
 	private GOval saaya;  //will change GOval to Gimage of cat later
 	
 	
+	
 	Timer crowtimer = new Timer(5, this);
-	int x=0, y=0, velx=0, vely=0;
+	int x=15, y=0, velx=0, vely=0;
 	
 	
 	public Crow(MainApplication app) {
@@ -29,9 +32,18 @@ public class Crow extends GraphicsPane implements ActionListener, KeyListener {
 		saaya = new GOval(START_X, START_Y, 100, 100);  //making start position static
 		saaya.setFilled(true);
 		saaya.setColor(Color.green);
+	
+		
 		
 	}
+
 	
+	//public void OutOfBounds() {
+		//double rightarea =  crowgamebackground.getWidth()- saaya.getSize();
+		
+//}
+
+
 	//public Crow() {
 		//crowtimer.start();
 		//addKeyListener(this);
@@ -62,6 +74,7 @@ public class Crow extends GraphicsPane implements ActionListener, KeyListener {
 	public void actionPerformed(ActionEvent e) {
 		x = x + velx;
 		y = y + vely;
+	
 		
 	} 
 	
@@ -70,9 +83,11 @@ public class Crow extends GraphicsPane implements ActionListener, KeyListener {
 		int c= e.getKeyCode();
 		
 		if (c == KeyEvent.VK_LEFT) {
+			
 			velx = -1;
 			vely=0;
-			saaya.move(-15, 0);
+			saaya.move(-15, 0); 
+	
 		}
 		
 		
@@ -94,6 +109,7 @@ public class Crow extends GraphicsPane implements ActionListener, KeyListener {
 			saaya.move(0, 1);
 		}
 		
+		 
 	}
 	
 	public void keyTyped(KeyEvent e) {}
