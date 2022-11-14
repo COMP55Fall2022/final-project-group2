ca09c7bca42f2f5003d3dc363491e2691aac3947
Sagar Mathur <s_mathur4@u.pacific.edu>
2022-11-14 14:21:13 -0800

    started ttt graphics class

---
+++ src/main/java/edu/pacific/comp55/starter/TTTGraphics.java
@@ -0,0 +1,11 @@
+package edu.pacific.comp55.starter;
+
+public class TTTGraphics extends TicTacToe{
+	 public TTTGraphics(int rows, int cols) {
+		super(rows, cols);
+		// TODO Auto-generated constructor stub
+	}
+	 public static final int PROGRAM_WIDTH = 500;
+	 public static final int PROGRAM_HEIGHT = 500;
+	 
+}
