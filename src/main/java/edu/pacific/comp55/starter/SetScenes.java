eabeccb39b5b46959cbd5da29aa1dff9236af0ff
ayeshashamsi <a_shamsi@u.pacific.edu>
2022-11-17 20:05:15 -0800

    added objects to set scenes class

--- src/main/java/edu/pacific/comp55/starter/SetScenes.java
+++ src/main/java/edu/pacific/comp55/starter/SetScenes.java
@@ -1,25 +1,31 @@
 package edu.pacific.comp55.starter;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+
+import javax.swing.Timer;
 
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GOval;
 
-public class SetScenes extends GraphicsPane{
+public class SetScenes extends GraphicsPane implements ActionListener{
 	private MainApplication program; 
 	private GImage scene1;
 	private GImage roses;
 	private GLabel yes;
+	Timer rosestimer = new Timer(40, this);
+	int roseCountdown = 0;
 	
 	public SetScenes(MainApplication app) {
 		super();
 		program = app;
 		scene1 = new GImage("scene1.png", 0, 0);
 		roses = new GImage("roses.png", -165, 178);
-		yes = new GLabel("You found it", 160, 170);
+		yes = new GLabel("This is not the main object, but keep trying!", 160, 170);
 	
 		
 		
@@ -49,16 +55,30 @@ public class SetScenes extends GraphicsPane{
 		
 	}
 	
-	public void addrosetext() {
-		program.add(yes);
-	}
+	
 	
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
 		if (obj == roses) {
-			addrosetext();
-		
+			rosestimer.start();
+			
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent g) {
+		Object source = g.getSource();
+		
+		if (source == rosestimer) {
+			roseCountdown++;
+			System.out.println(roseCountdown);
+			program.add(yes);
+			  if (roseCountdown > 100) {
+				 program.remove(yes);
+				 rosestimer.stop();
+			 }
+		}
+		
 	}
 	
 	
