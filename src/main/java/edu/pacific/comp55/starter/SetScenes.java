7132f2b936258c7b5057fc999aac64f881ac175e
ayeshashamsi <a_shamsi@u.pacific.edu>
2022-11-21 13:58:53 -0800

    added code for interactable objects for secene1/setscenes class

--- src/main/java/edu/pacific/comp55/starter/SetScenes.java
+++ src/main/java/edu/pacific/comp55/starter/SetScenes.java
@@ -12,76 +12,69 @@ import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GOval;
 
-public class SetScenes extends GraphicsPane implements ActionListener{
-	private MainApplication program; 
+public class SetScenes extends GraphicsPane implements ActionListener {
+	private MainApplication program;
 	private GImage scene1;
 	private GImage roses;
-	private GLabel yes;
+	private GImage rosesdialogue;
 	Timer rosestimer = new Timer(40, this);
 	int roseCountdown = 0;
-	
+
 	public SetScenes(MainApplication app) {
 		super();
 		program = app;
 		scene1 = new GImage("scene1.png", 0, 0);
 		roses = new GImage("roses.png", -165, 178);
-		yes = new GLabel("This is not the main object, but keep trying!", 160, 170);
-	
-		
-		
+//		rosetext = new GLabel("This is not the main object, but keep trying!", 160, 170);
+
 	}
-	
+
 	// if we win the game we switch to scene1
-	
+
 	@Override
 	public void showContents() {
 		program.add(scene1);
 		program.add(roses);
 
-		
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(scene1);
 		program.remove(roses);
-		
-	}
-	
 
-	
+	}
+
 	public void setscene1() {
 		program.add(scene1);
-		
+
 	}
-	
-	
-	
+
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
 		if (obj == roses) {
+			rosesdialogue = new GImage("rosesdialogue.png", 65, 200);
+			program.add(rosesdialogue );
 			rosestimer.start();
-			
+
 		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent g) {
 		Object source = g.getSource();
-		
+
 		if (source == rosestimer) {
 			roseCountdown++;
 			System.out.println(roseCountdown);
-			program.add(yes);
-			  if (roseCountdown > 100) {
-				 program.remove(yes);
-				 rosestimer.stop();
-			 }
+
+			if (roseCountdown > 100) {
+				program.remove(rosesdialogue);
+				roseCountdown = 0;
+				rosestimer.stop();
+			}
 		}
-		
+
 	}
-	
-	
-	
-	
+
 }
