a874159b2de6c76f2d8e8a97fdc894ab544c4eaa
ayeshashamsi <a_shamsi@u.pacific.edu>
2022-12-08 16:14:32 -0800

    changes

--- src/main/java/edu/pacific/comp55/starter/SetScene4.java
+++ src/main/java/edu/pacific/comp55/starter/SetScene4.java
@@ -17,33 +17,52 @@ public class SetScene4 extends GraphicsPane implements ActionListener {
 	private GImage scene4;
 	
 	//Gimages needed for scene4
+	private GImage saaya;
 	private GImage dog3;
 	private GImage dog3text;
+	private GImage apple;
+	private GImage appletext;
 	private GImage basket;
 	private GImage basketscreen;
 	private GImage mapGif;
 	private AudioPlayer mapSound;
 	public static final String MUSIC_FOLDER = "sounds";
 	private static final String SOUND_FILE = "Running On Leaves.mp3";
+	
+	private GButton cont;
+	private GButton exit;
+	
 	//Timers needed for scene 4
 	private boolean baskettimerstart = false;
 	Timer dog3timer = new Timer(40, this);
+	Timer appletimer = new Timer(40, this);
 	Timer baskettimer = new Timer(40, this);
 	Timer gifTimer = new Timer(1000, this);
 	int dialogueCountdown = 0;
 	private int gifCounter = 0;
+	
 	public SetScene4(MainApplication app) {
 		super();
 		program = app;
 		
 		//For scene 4
-		scene4 = new GImage("scene4.png", 0, 0);
-		dog3 = new GImage("dog3.png", 200, 200);
-		dog3text = new GImage("dog3text.png", 10, 10);
-		basket = new GImage("basket.png", 0, 0);
+		saaya = new GImage("cat.png", 200, 600);
+		scene4 = new GImage("basketbackground.png", 0, 0);
+		dog3 = new GImage("dog3.png", 700, 500);
+		apple = new GImage("apple.png", 600, 700);
+		basket = new GImage("basket.png", 400, 600);
 		basketscreen = new GImage("basketscreen.png", 0 ,0);
 		mapGif = new GImage("journey3.gif",0,0);
 		mapSound = AudioPlayer.getInstance();
+
+		dog3.scale(0.4);
+		basket.scale(0.25);
+		apple.scale(0.2);
+		
+		cont = new GButton("Continue", 655, 600, 100, 100);
+		cont.setFillColor(Color.GREEN);
+		exit = new GButton("Exit", 455, 600, 100, 100);
+		exit.setFillColor(Color.RED);
 	}
 	
 	// if we win the game we switch to scene4
@@ -63,6 +82,9 @@ public class SetScene4 extends GraphicsPane implements ActionListener {
 		program.add(scene4);
 		program.add(dog3);
 		program.add(basket);
+		program.add(apple);
+		program.add(saaya);
+		program.add(mapGif);
 	}
 	
 	// These functions check if the user is clicking on the interactable object and 
@@ -70,6 +92,33 @@ public class SetScene4 extends GraphicsPane implements ActionListener {
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
 		
+		
+		if (obj == dog3 && !baskettimerstart) {
+			dog3text = new GImage("dog3text.png", 380, 300);
+			dog3text.scale(0.5);
+			program.add(dog3text);
+			dog3timer.start();
+		}
+		
+		if (obj == apple && !baskettimerstart) {
+			appletext = new GImage("appletext.png", 570, 500);
+			appletext.scale(0.5);
+			program.add(appletext);
+			appletimer.start();
+		}
+		
+		if (obj == basket) {
+			basketscreen = new GImage("basketscreen.png", 350, 200);
+			basketscreen.scale(0.5);
+			program.add(basketscreen);
+			baskettimer.start();
+			baskettimerstart = true;
+		}
+		
+		if (obj == cont) {
+			removebuttons();
+			program.switchToBasket();
+		}
 	
 	}
 	
@@ -88,28 +137,43 @@ public class SetScene4 extends GraphicsPane implements ActionListener {
 			}
 		}
 		
-	//	if (source == rosestimer) {
-	//		dialogueCountdown++;
-	//		System.out.println(dialogueCountdown);
-	//		if (dialogueCountdown > 100) {
-	//			program.remove(rosesdialogue);
-	//			dialogueCountdown = 0;
-	//			rosestimer.stop();
-	//		}
-	//	}
+		if (source == dog3timer) {
+			dialogueCountdown++;
+			if (dialogueCountdown > 100) {
+				program.remove(dog3text);
+				dialogueCountdown = 0;
+				dog3timer.stop();
+			}
+		}
 		
-	//	if (source == blueflowertimer) {
-	//		dialogueCountdown++;
-			
-	//		if (dialogueCountdown > 100) {
-	//			program.remove(blueflowertext);
-	//			dialogueCountdown = 0;
-	//			blueflowertimer.stop();
-	//		}
-	//	}
+		if (source == appletimer) {
+			dialogueCountdown++;
+			if (dialogueCountdown > 100) {
+				program.remove(appletext);
+				dialogueCountdown = 0;
+				appletimer.stop();
+			}
+		}
 		
-	//	if (source == suntimer) {
-			
-	//	}
+		if (source == baskettimer) {
+			baskettimer.stop();
+			program.add(cont);
+			program.add(exit);
+		}
+		
+		if(source == gifTimer) {
+			gifCounter++;
+			System.out.println(gifCounter);
+			if(gifCounter == 3) {
+				program.remove(mapGif);
+				gifTimer.stop();
+			}
+		}
+
+	}
+	
+	public void removebuttons() {
+		program.remove(cont);
+		program.remove(exit);
 	}
 }
